e60157557f04b88dee7e97b7cf2f199cd4de9f00
benzen89 <salomaa.leo@gmail.com>
2020-09-22 19:08:06 +0300

    Change cities

---
+++ Assets/Scripts/city_activator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class city_activator : MonoBehaviour
+{
+    public GameObject[] cities;
+
+    public void ChangeCity(int cityIndex)
+    {
+        for(int i = 0; i < cities.Length; i++)
+        {
+            cities[i].SetActive(false);
+        }
+        cities[cityIndex].SetActive(true);
+    }
+
+}
