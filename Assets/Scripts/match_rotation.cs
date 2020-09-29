21845cd6930a3d20e627c33e92e82f5fc0d69ba6
benzen89 <salomaa.leo@gmail.com>
2020-09-29 15:01:45 +0300

    Rotate 360 camera by touch controlls

---
+++ Assets/Scripts/match_rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class match_rotation : MonoBehaviour
+{
+    
+    public GameObject modelObj;
+
+    void Update()
+    {
+        transform.rotation = modelObj.transform.rotation;
+    }
+}
