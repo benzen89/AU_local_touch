891c5500597f30811e7016bf939fb02e8519630d
benzen89 <salomaa.leo@gmail.com>
2020-09-29 15:19:34 +0300

    Stop video & Video brigthness

---
+++ Assets/Scripts/stop_video.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class stop_video : MonoBehaviour
+{
+    GameObject playbackSphere;
+
+    void Start()
+    {
+        playbackSphere = GameObject.Find("VideoPlaybackSphere");
+    }
+
+    public void StopVideo(){
+        VideoPlayer vp = playbackSphere.GetComponent<VideoPlayer>();
+        vp.clip = null;
+        vp.Stop();
+    }
+}
