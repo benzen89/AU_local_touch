13cba3c743a8e86ff620b14b95c9574022e8ee51
benzen89 <salomaa.leo@gmail.com>
2020-09-29 14:54:37 +0300

    Full screen views for AU & ? buttons

---
+++ Assets/Scripts/ui_panel_activator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ui_panel_activator : MonoBehaviour
+{
+    
+    public void OpenPanel(GameObject panel){
+        panel.SetActive(true);
+    }
+
+    public void ClosePanel(GameObject panel){
+        panel.SetActive(false);
+    }
+
+}
