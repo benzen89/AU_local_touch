bfca7b63f1d53e7c3a8a2db41f7e11f5353bdeb7
benzen89 <salomaa.leo@gmail.com>
2020-10-13 15:27:05 +0300

    Change language functionality

---
+++ Assets/Scripts/alternative_languages.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alternative_languages : MonoBehaviour
+{
+
+    public List<GameObject> languages;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateLanguage();
+    }
+
+    public void UpdateLanguage(){
+        //check active language
+        int activeLanguage = GameObject.Find("Canvas").GetComponent<language_change>().activeLanguage;
+
+        //disable all language prefabs
+        for (int i = 0; i < languages.Count; i++){
+            languages[i].SetActive(false);
+        }
+        
+        //enable active language prefab
+        languages[activeLanguage].SetActive(true);
+    }
+
+}
