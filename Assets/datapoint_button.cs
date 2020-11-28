e945384ea646e54fb3ef9b63dabb226a91081dfc
Leo Virolainen <41674331+LeoVirolainen@users.noreply.github.com>
2020-11-28 19:09:01 +0200

    Images on minimap canvas + tallinn & viimsi info updates

--- Assets/datapoint_button.cs
+++ Assets/datapoint_button.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class datapoint_button : MonoBehaviour
 {
     info_panel_change info_Panel_Change;
     public GameObject newInfo;
+    Image infoDisplay;
+    public Sprite newSprite;
 
     private void Start()
     {
         info_Panel_Change = GameObject.Find("Canvas").GetComponent<info_panel_change>();
+        infoDisplay = GameObject.Find("info_image_display").GetComponent<Image>();
     }
     private void OnMouseDown()
     {
         info_Panel_Change.ChangeInfoPanel(newInfo);
+        if (newSprite != null) //set newSprite as the new image on viewer, if a sprite has been specified
+        {
+            infoDisplay.sprite = newSprite;
+        }
+        else //disable the image component if a sprite hasn't been specified
+        {
+            infoDisplay.enabled = false;
+        }
+        
+        if (infoDisplay.enabled == false && newSprite != null) //enable the image component if off and if a sprite has been specified
+        {
+            infoDisplay.enabled = true;
+        }
     }
 }
