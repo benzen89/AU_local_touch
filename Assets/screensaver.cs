0996c101906c01b757ffbf8ae1301a3f9a44a352
Leo Virolainen <41674331+LeoVirolainen@users.noreply.github.com>
2020-11-28 22:31:26 +0200

    screensaver

---
+++ Assets/screensaver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screensaver : MonoBehaviour
+{
+    public float timer;
+    public GameObject _AUPanel;
+    public float secondsToScreenSaver = 50f;
+    void Start()
+    {
+        timer = Time.time + secondsToScreenSaver;
+    }
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            timer = Time.time + secondsToScreenSaver;
+        }
+        if (Time.time > timer)
+        {
+            _AUPanel.SetActive(true);
+        }
+    }
+}
