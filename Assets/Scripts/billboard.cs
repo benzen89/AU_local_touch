1a37a3dbceef13a220224508fee3a4436484f69e
benzen89 <salomaa.leo@gmail.com>
2020-11-30 10:21:23 +0200

    Billboard script added

---
+++ Assets/Scripts/billboard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class billboard : MonoBehaviour
+{
+    public bool invert = false;
+
+    Transform mainCameraTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+        //Debug.Log(mainCameraTransform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(invert){
+            transform.LookAt(transform.position - mainCameraTransform.position);
+        } else{
+            transform.LookAt(mainCameraTransform);
+        }
+    }
+}
