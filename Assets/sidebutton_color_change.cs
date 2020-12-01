a4e97275ff79c101dcb04d0ba494b2a8494a9e5c
Leo Virolainen <41674331+LeoVirolainen@users.noreply.github.com>
2020-12-01 19:04:58 +0200

    2D video player

---
+++ Assets/sidebutton_color_change.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sidebutton_color_change : MonoBehaviour
+{
+    GameObject[] sideButtons;
+    public Sprite activeImage;
+    public Sprite inactiveImage;
+    private void Start()
+    {
+        sideButtons = GameObject.FindGameObjectsWithTag("SideButton");
+    }
+    public void Change_btn_color()
+    {
+        foreach (GameObject button in sideButtons)
+        {
+            button.GetComponent<Image>().sprite = button.GetComponent<sidebutton_color_change>().inactiveImage;
+        }
+        transform.GetComponent<Image>().sprite = activeImage;
+    }
+}
+
