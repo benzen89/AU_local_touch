683693695039dcd371bb3e2f87ccccc71e88c9ed
Leo Virolainen <41674331+LeoVirolainen@users.noreply.github.com>
2020-12-01 23:20:56 +0200

    replaced mask with rect mask 2D component to rectify mask not porting to build

---
+++ Assets/scrollRectEnabler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scrollRectEnabler : MonoBehaviour
+{
+
+    public ScrollRect scrollRect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollRect = GameObject.Find("Scroll View").GetComponent<ScrollRect>();
+        scrollRect.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
